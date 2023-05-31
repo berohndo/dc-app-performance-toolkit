8bca687f837395b0d6b0ca74cf3655908c725a95
Bernd Öhninger <bernd.oehninger@linuxit.at>
2023-05-31 14:48:13 +0200

    add app specific tests

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -39,6 +39,6 @@ def app_specific_action(webdriver, datasets):
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CSS_SELECTOR, '[data-macro-name="viable-tile"]'))
+            page.wait_until_visible((By.CSS_SELECTOR, '[data-macro-name="state-swapper-macro"]'))
             sub_measure()
     measure()
